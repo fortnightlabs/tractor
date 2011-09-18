64135b443b1056ce9c5c0862c82632c51efb932f
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-09-17 17:23:54 -0700

    better timestamp formatting

--- console.js
+++ console.js
@@ -5,6 +5,7 @@ var repl = require('repl')
   , context = repl.start().context;
 
 context.app = require('./config/application');
+context.Date = Date;
 context.Item = context.app.db.model('Item');
 
 process.stdin.on('close', function() {
