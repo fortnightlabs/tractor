b6903710f1fe580bf3c54b01a961bdbf2caa9bdb
Gerad Suyderhoud <gerad@fortnightlabs.com>
2011-09-22 18:29:47 -0700

    handle delays in recording and sleeping

--- macapp/Tractor/TractorController.m
+++ macapp/Tractor/TractorController.m
@@ -11,7 +11,13 @@ static int64_t SystemIdleSeconds(void);
 
 - (void)insertOrUpdateItemWithName:(NSString *)name
                            andInfo:(NSDictionary *)info
-                            atTime:(NSDate *)now;
+                           atStart:(NSDate *)start;
+
+- (NSDate *)idleAt;
+- (NSDate *)delayedAt;
+
+- (void)receiveWillSleepNotification:(NSNotification*)notification;
+- (void)observeWillSleepNotification;
 
 - (void)updateLatestItemFromDataStore;
 
@@ -25,6 +31,9 @@ static int64_t SystemIdleSeconds(void);
   if (self) {
     context = [managedObjectContext retain];
     informer = [[CurrentApplicationInformer alloc] init];
+
+    [self observeWillSleepNotification];
+    
     [self updateLatestItemFromDataStore];
     [self checkCurrentState:nil];
   }
@@ -34,29 +43,34 @@ static int64_t SystemIdleSeconds(void);
 
 - (void)checkCurrentState:(NSTimer *)timer
 {
-  int64_t idleSeconds = SystemIdleSeconds();
+  NSDate *start = nil;
+  NSDate *idleAt;
+  NSDate *delayedAt;
   NSString *name = nil;
   NSDictionary *info = nil;
-  NSDate *now = [NSDate date];
 
-  if (idleSeconds > 5 * 60) { // 5 minutes hard coded for now
-    now = [NSDate dateWithTimeIntervalSinceNow:(NSTimeInterval)idleSeconds];
+  if ((idleAt = [self idleAt])) {
+    start = idleAt;
+  } else if ((delayedAt = [self delayedAt])) {
+    start = delayedAt;
   } else {
     CurrentApplicationInfo *app = [informer currentApplicationInfo];
     name = [app name];
     info = [app info];
+    start = [NSDate date];
   }
   [self insertOrUpdateItemWithName:name
                            andInfo:info
-                            atTime:now];  
+                           atStart:start];
   
   [self checkStateAgainInOneSecond];
 }
 
 - (void)insertOrUpdateItemWithName:(NSString *)name
                            andInfo:(NSDictionary *)info
-                            atTime:(NSDate *)now;
+                           atStart:(NSDate *)start;
 {
+  NSDate *now = [NSDate date];
   NSData *infoData = [info JSONData];
 
   // TODO handle when now is before start (can happen due to idle time)
@@ -64,15 +78,15 @@ static int64_t SystemIdleSeconds(void);
   if ([latestItem matchesApp:name andInfoData:infoData]) {
     [latestItem setEnd:now]; // update the end time
   } else {
-    [latestItem setEnd:now]; // update the previous item's end time
+    [latestItem setEnd:start]; // update the previous item's end time
     
     // insert a new item
-    [latestItem autorelease];
+    [latestItem release];
     latestItem = [NSEntityDescription insertNewObjectForEntityForName:@"Item"
                                            inManagedObjectContext:context];
     [latestItem retain];
 
-    [latestItem setStart:now];
+    [latestItem setStart:start];
     [latestItem setEnd:now];
 
     if (name) { [latestItem setApp:name]; }
@@ -84,6 +98,55 @@ static int64_t SystemIdleSeconds(void);
     NSLog(@"Couldn't save: %@", [error localizedDescription]);
   }
 }
+
+// returns the time when the computer became idle, nil if not idle
+- (NSDate *)idleAt
+{
+  NSDate *ret = nil;
+  int64_t idleSeconds = SystemIdleSeconds();
+  
+  // you only become "idle" if you haven't done anything for 5 minutes
+  // and once you become idle, we backdate the time it happens
+  if (idleSeconds > 5 * 60) { // hardcoded for now
+    ret = [NSDate dateWithTimeIntervalSinceNow:-(NSTimeInterval)idleSeconds];
+  }
+  
+  return ret;
+}
+
+// if an update hasn't occurred in a while, for whatever reason, note it
+- (NSDate *)delayedAt
+{
+  NSDate *ret = nil;
+  NSDate *lastEnd = [latestItem end];
+  NSTimeInterval secondsSinceLastUpdate = -[lastEnd timeIntervalSinceDate:[NSDate date]];
+
+  // if no updates have happened in the last 60 seconds, assume it
+  // became delated at the last update time
+  if (secondsSinceLastUpdate > 60) {
+    ret = lastEnd;
+  }
+  
+  return ret;
+}
+
+// http://stackoverflow.com/questions/6089096/knowing-when-the-system-has-gone-to-sleep-in-a-menu-extra
+- (void)receiveWillSleepNotification:(NSNotification*)notification
+{
+  // record that the computer is going to sleep
+  [self insertOrUpdateItemWithName:nil
+                           andInfo:nil
+                           atStart:[NSDate date]];
+}
+
+- (void)observeWillSleepNotification
+{
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                         selector:@selector(receiveWillSleepNotification:)
+                                                             name:NSWorkspaceWillSleepNotification
+                                                           object:nil];
+}
+
 
 - (void)updateLatestItemFromDataStore
 {
